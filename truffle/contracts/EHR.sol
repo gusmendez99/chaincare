117ae4d687ebd40b9f5e9d43dfae6bdb7b6a983d
Gus Mendez <gus.mendez.99@gmail.com>
2022-09-26 00:27:58 -0600

    [FIX] solidity contract changes

--- truffle/contracts/EHR.sol
+++ truffle/contracts/EHR.sol
@@ -2,104 +2,101 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract EHR { 
-  // Constants
-  string constant NO_EXISTS_SENDER_ERROR = "Sender does not exist";
-  string constant NO_EXISTS_PATIENT_ERROR = "Patient does not exist";
-  string constant NO_DOCTOR_ROLE_ERROR = "Sender is not a doctor";
-  string constant EXISTS_PATIENT_ERROR = "This patient already exists.";
-  string constant EXISTS_DOCTOR_ERROR = "This doctor already exists.";
-  string constant EXISTS_RECORD_ERROR = "The record with this ID already exists.";
-
-  string constant DOCTOR_ROLE = "doctor";
-  string constant PATIENT_ROLE = "patient";
-  string constant UNKNOWN_ROLE = "unknown";
-
-
-
   // Models
   struct Record { 
     string id;
     string filename; 
-    address patientId;
-    address doctorId;
+    address idPatient;
+    address idDoctor;
     uint256 createdAt;
+  }
+  
+  struct Doctor {
+    address id;
   }
 
   struct Patient {
     address id;
-    mapping (string => Record) records;
+    Record[] records;
   }
 
-  struct Doctor {
-    address id;
-  }
 
   // Mappings & Events
   mapping (address => Patient) public patients;
   mapping (address => Doctor) public doctors;
 
-  event RecordAdded(string id, address patientId, address doctorId); 
-  event PatientAdded(address patientId);
-  event DoctorAdded(address doctorId);
+  event RecordAdded(string id, address idPatient, address idDoctor); 
+  event PatientAdded(address idPatient);
+  event DoctorAdded(address idDoctor);
 
   // Modifiers
   modifier senderExists {
     require(
       doctors[msg.sender].id == msg.sender || patients[msg.sender].id == msg.sender,
-      NO_EXISTS_SENDER_ERROR
+      "Sender does not exist"
     );
     _;
   }
 
-  modifier patientExists(address patientId) {
-    require(patients[patientId].id == patientId, NO_EXISTS_PATIENT_ERROR);
+  modifier patientExists(address idPatient) {
+    require(
+      patients[idPatient].id == idPatient,
+      "Patient does not exist"
+    );
     _;
   }
 
   modifier senderIsDoctor {
-    require(doctors[msg.sender].id == msg.sender, NO_DOCTOR_ROLE_ERROR);
+    require(
+      doctors[msg.sender].id == msg.sender,
+      "Sender is not a doctor"
+    );
     _;
   }
 
   // Functions
-  function addPatient(address _patientId) public senderIsDoctor {
-    require(patients[_patientId].id != _patientId, EXISTS_PATIENT_ERROR);
-    patients[_patientId].id = _patientId;
+  function addPatient(address _idPatient) public senderIsDoctor {
+    require(
+      patients[_idPatient].id != _idPatient,
+      "Patient with this ID already exists"
+    );
+    patients[_idPatient].id = _idPatient;
 
-    emit PatientAdded(_patientId);
+    emit PatientAdded(_idPatient);
   }
 
   function addDoctor() public {
-    require(doctors[msg.sender].id != msg.sender, EXISTS_DOCTOR_ERROR);
+    require(
+      doctors[msg.sender].id != msg.sender,
+      "Doctor with this ID already exists"
+    );
     doctors[msg.sender].id = msg.sender;
 
     emit DoctorAdded(msg.sender);
   }
 
-  function addRecord(string memory _id, string memory _filename, address _patientId) public senderIsDoctor patientExists(_patientId) {
-    require(
-      patients[_patientId].records[_id].createdAt == 0,
-      EXISTS_RECORD_ERROR
-    );
+  function addRecord(string memory _id, string memory _filename, address _idPatient) public senderIsDoctor patientExists(_idPatient) {
+    Record memory record = Record(_id, _filename, _idPatient, msg.sender, block.timestamp);
+    patients[_idPatient].records.push(record);
 
-    Record memory record = Record(_id, _filename, _patientId, msg.sender, block.timestamp); 
-    patients[_patientId].records[_id] = record; 
-
-    emit RecordAdded(_id, _patientId, msg.sender); 
-  } 
-
-  function getRecord(string memory _id, address _patientId) public view senderExists patientExists(_patientId) returns (string memory, string memory, address, address, uint256) { 
-    Record memory record = patients[_patientId].records[_id]; 
-    return (record.id, record.filename, record.patientId, record.doctorId, record.createdAt); 
+    emit RecordAdded(_id, _idPatient, msg.sender); 
   }
+
+  function getRecords(address _idPatient) public view senderExists patientExists(_idPatient) returns (Record[] memory) {
+    return patients[_idPatient].records;
+  } 
 
   function getSenderRole() public view returns (string memory) {
     if (doctors[msg.sender].id == msg.sender) {
-      return DOCTOR_ROLE;
+      return "doctor";
     } else if (patients[msg.sender].id == msg.sender) {
-      return PATIENT_ROLE;
+      return "patient";
     } else {
-      return UNKNOWN_ROLE;
+      return "unknown";
     }
+  }
+
+  function getPatientExists(address _idPatient) public view senderIsDoctor returns (bool) {
+    return patients[_idPatient].id == _idPatient;
   }
 } 
